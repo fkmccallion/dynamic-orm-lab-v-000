d46d43e0b9810764a031ff99944c7b8fdb114115
Frank McCallion <fkmccallion@gmail.com>
2018-03-28 03:53:27 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,12 @@
 require 'sqlite3'
-require_relative "../Gemfile"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 DB[:conn].execute("DROP TABLE IF EXISTS students")
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY, 
-  name TEXT, 
+  id INTEGER PRIMARY KEY,
+  name TEXT,
   grade INTEGER
   )
 SQL
